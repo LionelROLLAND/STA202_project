bf1804d04711509afa2243b930d33e5e32dc1e48
Rakotomandimby Seta <seta.rakotomandimby@ensta-paris.fr>
2022-02-28 16:13:23 +0100

    Ajout d'un fichier séparé pour le traitement en série temporelle de nos données

---
+++ Traitement_TS.R
@@ -0,0 +1,6 @@
+rm(list=ls())
+setwd("~/.STA202_BDD/STA202_project/BDD/")
+
+acc_grav_pday=readRDS(file="acc_grav_pday.rds")
+acc_lege_pday=readRDS(file="acc_lege_pday.rds")
+days=readRDS(file="days.rds")
